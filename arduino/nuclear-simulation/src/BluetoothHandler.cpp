82fbfdc9d5b16d21067fc84f46ad32bd87a56d78
Lucas Arvidsson <Lucas.arvidsson@hotmail.com>
2019-09-19 21:51:51 +0200

    ADD: Function for sending radiation value

--- arduino/nuclear-simulation/src/BluetoothHandler.cpp
+++ arduino/nuclear-simulation/src/BluetoothHandler.cpp
@@ -18,14 +18,25 @@ void btLoop() {
 
 }
 
-void btSendData(int protocol, String uniqueID, int clockedStatus) {
-hc06.print(protocol);
-hc06.print(",");
-hc06.print(uniqueID);
-hc06.print(",");
-hc06.print(clockedStatus);
-hc06.print(";");
-delay(20);
 
+void btSendData(int protocol, String uniqueID, int clockedStatus) {
+  hc06.print(protocol);
+  hc06.print(",");
+  hc06.print(uniqueID);
+  hc06.print(",");
+  hc06.print(clockedStatus);
+  hc06.print(",");
+  delay(20);
+
+}
+
+void btSendRadData(int protocol, String uniqueID, float radValue) {
+  hc06.print(protocol);
+  hc06.print(",");
+  hc06.print(uniqueID);
+  hc06.print(",");
+  hc06.print(radValue);
+  hc06.print(";");
+  delay(20);
 }
 
